029844bf3669e66ba53a8a608a827f4572b28d86
@JSDunn105 <j.s.dunn@icloud.com>
2019-02-19 14:39:23 -0500

    It's a kind of wedding...you don't want to be the groom

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    "My Sinatra/Shotgun app is doin' its THANG!"
   end
 
 end
